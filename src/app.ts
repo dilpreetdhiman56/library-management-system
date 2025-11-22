ac4ac9a88b69b40359f8b9c0cb1735ed2fc883d1
Dilpreet singh <dilpreetsingh6@rrc.ca>
2025-11-22 15:53:35 -0600

    Setup swagger.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import bookRoutes from "./api/v1/routes/bookRoutes";
 import borrowRoutes from "./api/v1/routes/borrowRoutes";
 import userRoutes from "./api/v1/routes/userRoutes";
+import setupSwagger from "../config/swagger";
 
 // Initialize Express application
 const app: Express = express();
@@ -17,4 +18,5 @@ app.use("/api/v1", bookRoutes)
 app.use("/api/v1", borrowRoutes)
 app.use("/api/v1", userRoutes)
 
+setupSwagger(app);
 export default app;
