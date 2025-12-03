37ec33e7aa816f3300868a447ffd7b3854945e7b
Dilpreet singh <dilpreetsingh6@rrc.ca>
2025-12-03 00:44:51 -0600

    Added global erro handler.

--- config/fireBaseConfig.ts
+++ config/fireBaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-
+import { getAuth, Auth } from "firebase-admin/auth";
 import * as serviceAccount from "./key.json";
 
 // Initialize the Firebase app with the service account credentials
@@ -9,6 +9,7 @@ initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
 });
 
+const auth: Auth = getAuth();
 // Get a reference to the Firestore service
 // This creates a Firestore instance that you can use to interact with your database
 const db: Firestore = getFirestore();
